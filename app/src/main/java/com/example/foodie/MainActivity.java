58cbde64188aab07f6b9c837c3f370bafe5b9d31
Iman Ahsan <iman.ahsan10@gmail.com>
2022-06-02 19:36:37 -0400

    added tabs

--- app/src/main/java/com/example/foodie/MainActivity.java
+++ app/src/main/java/com/example/foodie/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.foodie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //im gay
-        // yes u are
+        // references the tabLayout and viewPager
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager);
+
+
+        // creates a pageradapter, which allows the view pager to allow swiping on fragments
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+
+        // mediator connects the tab to the fragment
+        new TabLayoutMediator(tabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+              switch (position) {
+                  case 0:
+                      tab.setText("Adnan");
+                      break;
+                  case 1:
+                      tab.setText("Iman");
+                      break;
+                  default:
+                      tab.setText("Thenuja");
+                      break;
+              }
+
+            }
+        }).attach();
     }
+
 }
