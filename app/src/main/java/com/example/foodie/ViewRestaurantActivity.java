f58a350e97427fb981ac13b7cbed9ecee4e21641
AdnxnHabib <adnanhabib263@gmail.com>
2022-06-09 15:17:36 -0400

    added go back button

--- app/src/main/java/com/example/foodie/ViewRestaurantActivity.java
+++ app/src/main/java/com/example/foodie/ViewRestaurantActivity.java
@@ -25,7 +25,7 @@ public class ViewRestaurantActivity extends AppCompatActivity {
     private TextView name2;
     private RatingBar ratingBar;
     private CheckBox distanceBox1, distanceBox2, distanceBox3;
-    private Button deleteRestaurantBtn;
+    private Button deleteRestaurantBtn, goBackBtn;
     private DBHandler dbHandler;
     String name, order, cuisine, distance;
     float rating;
@@ -44,6 +44,7 @@ public class ViewRestaurantActivity extends AppCompatActivity {
         distanceBox3 = findViewById(R.id.option3CBdetail);
         cuisineTV = findViewById(R.id.cuisineETdetail);
         deleteRestaurantBtn = findViewById(R.id.delete);
+        goBackBtn = findViewById(R.id.btnEditCancel);
 
         // initialing our dbhandler class.
         dbHandler = new DBHandler(ViewRestaurantActivity.this);
@@ -76,7 +77,7 @@ public class ViewRestaurantActivity extends AppCompatActivity {
         }
         cuisineTV.setText(cuisine);
 
-        // adding on click listener to our update course button.
+        // adding on click listener to our delete restaurant button.
         deleteRestaurantBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,79 +87,26 @@ public class ViewRestaurantActivity extends AppCompatActivity {
                 dbHandler.deleteRestaurant(name);
 
                 // displaying a toast message that our course has been updated.
-                Toast.makeText(ViewRestaurantActivity.this, "Restaurant Deleted..", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ViewRestaurantActivity.this, "Restaurant Deleted...", Toast.LENGTH_SHORT).show();
 
                 // launching our main activity.
                 Intent i = new Intent(ViewRestaurantActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
+
+        // adding on click listener to our go back  button.
+        goBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // launching our main activity.
+                Intent i = new Intent(ViewRestaurantActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View view =  inflater.inflate(R.layout.activity_view_restaurant, container, false);
-
-//        // initializing all our variables.
-//        nameTV =  findViewById(R.id.nameETdetail);
-//        orderTV = findViewById(R.id.orderETdetail);
-//        ratingBar = findViewById(R.id.MyRatingdetail);
-//        distanceBox1 = findViewById(R.id.option1CBdetail);
-//        distanceBox2 = findViewById(R.id.option2CBdetail);
-//        distanceBox3 = findViewById(R.id.option3CBdetail);
-//        cuisineTV = findViewById(R.id.cuisineETdetail);
-//
-//
-//        // initialing our dbhandler class.
-//        dbHandler = new DBHandler(ViewRestaurantActivity.this);
-//
-//        // getting data which we passed in our adapter class.
-//        name = getIntent().getStringExtra("name");
-//        order = getIntent().getStringExtra("order");
-//        rating = getIntent().getIntExtra("rating", 0);
-//        distance = getIntent().getStringExtra("distance");
-//        cuisine = getIntent().getStringExtra("cuisine");
-//
-//
-//        // setting data to edit text of our update activity.
-//        nameTV.setText(name);
-//        orderTV.setText(order);
-//        ratingBar.setRating(rating);
-//        if (distance.contains("10-30")){
-//            distanceBox1.setChecked(false);
-//            distanceBox2.setChecked(true);
-//            distanceBox3.setChecked(false);
-//        }
-//        else if (distance.contains("10")){
-//            distanceBox1.setChecked(true);
-//            distanceBox2.setChecked(false);
-//            distanceBox3.setChecked(false);
-//        } else {
-//            distanceBox1.setChecked(false);
-//            distanceBox2.setChecked(false);
-//            distanceBox3.setChecked(true);
-//        }
-//        cuisineTV.setText(cuisine);
-//
-//        // adding on click listener to our update course button.
-//        deleteRestaurantBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                // inside this method we are calling an update course
-//                // method and passing all our edit text values.
-//                dbHandler.deleteRestaurant(name);
-//
-//                // displaying a toast message that our course has been updated.
-//                Toast.makeText(ViewRestaurantActivity.this, "Restaurant Deleted..", Toast.LENGTH_SHORT).show();
-//
-//                // launching our main activity.
-//                Intent i = new Intent(ViewRestaurantActivity.this, MainActivity.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        return view;
-//    }
 }
 
